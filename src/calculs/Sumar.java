52f555c79cf0749d51492e353a0e92b31ff0a1de
inaki <inaki@inaki-PC>
2015-02-18 20:07:41 +0100

    Commit2

---
+++ src/calculs/Sumar.java
@@ -0,0 +1,20 @@
+package calculs;
+
+/**
+ * <b>Mòdul que suma dos nombres</b>
+ * @version 1.0
+ * @author Grup 1
+ */
+public class Sumar {
+
+    /**
+     *
+     * @param x primer valor
+     * @param y segon valor
+     * @return int resultat x+y
+     */
+    public static int suma(int x,int y){
+            return x + y;
+        }
+    
+}
